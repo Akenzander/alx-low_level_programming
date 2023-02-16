1c7c11f8f539773c4c18ffacc666e93a212a1936
akenzander <alexenderwambua2001@gmail.com>
2023-02-16 08:35:49 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf fuction
+ * Return:0
+ */
+
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art,\n");
+return (0);
+}
