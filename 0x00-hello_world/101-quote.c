c5eb24c4fcc946c7e3a8d58ddd00d2b746be9f99
akenzander <alexenderwambua2001@gmail.com>
2023-02-16 09:50:11 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return 1
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	fput("and that piece of art is useful\n" - Dora Korpar, 2015 - 10 - 19\n", stdout);
+return: (1);
 }
 
