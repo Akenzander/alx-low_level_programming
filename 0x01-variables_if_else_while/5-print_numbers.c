321fec4738b3379f1adcf8d4702cc0b454afa593
akenzander <alexenderwambua2001@gmail.com>
2023-02-17 10:56:40 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+		for (a = 0; a < = 10; a++)
+			putchar(a);
+	putchar('\n');
+	return (0);
+}
