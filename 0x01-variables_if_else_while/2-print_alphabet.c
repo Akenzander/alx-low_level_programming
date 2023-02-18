ed4e88ad1d014d835c275324a3be4616642a8c39
akenzander <alexenderwambua2001@gmail.com>
2023-02-18 09:10:16 -0800

    3alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,18 +3,15 @@
 /**
  * main - returns alphabets
  *
- * Return: Always  0 (success)
+ * Return: Always  (0) (success)
  */
 int main(void)
 {
 	char c;
- 
-
-       for  (c = 'a' ; c <= 'z' ; c++)
+for (c = 'a' ; c <= 'z' ; c++)
 	{
 		putchar(c);
-	
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++
+			{
+			putchar(c);
+			}
+
+			for (c = 'A' ; c <= 'Z' ; c++
+				{
+				putchar(c);
+				}
+				putchar('\n');
+				return (0);
+}
+
