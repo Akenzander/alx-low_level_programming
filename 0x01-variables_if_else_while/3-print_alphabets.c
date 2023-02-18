f1906fa4490c6b3c597ddd034a65bfd3579f302b
akenzander <alexenderwambua2001@gmail.com>
2023-02-18 09:23:42 -0800

    alphabet3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,16 +9,32 @@
 
 int main(void)
 {
+
 	char c;
 
+
 	for (c = 'a' ; c <= 'z' ; c++
+
+	    )
 			{
+
+
+
 			putchar(c);
+
+			
 			}
 
+
 			for (c = 'A' ; c <= 'Z' ; c++
-				{
+
+			    )
+				
+			{
+
 				putchar(c);
+
+				
 				}
 				putchar('\n');
 				return (0);
