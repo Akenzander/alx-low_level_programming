177bacccea8a3c5d8f0be3b78cd462ed0d89bfa2
akenzander <alexenderwambua2001@gmail.com>
2023-02-22 09:43:43 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,43 @@
 #include "main.h"
+
 /**
- * print_times_table - Entry point
- * @n: input
- *
- * Return: Always 0 (success)
+ * print_times_table - -prints the times table of the input,
+ *           starting with 0.
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-	return (0);
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+_putchar('0');
+		for (num = 1; mult <= n; mult++)
+		{
+			_putchar(',');
+
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 99)
+
+				_putchar(' ');
+
+			if (prod >= 100)
+			{
+				_putchar((prod / 100) + '0');
+				_putchar(((prod / 10)) % 10 + '0');
+			}
+			else if (prod <= 99 && prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
 }
