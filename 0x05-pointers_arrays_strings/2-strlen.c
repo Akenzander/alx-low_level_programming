e701662ee9d00acf71a1439c04876ec722a43e28
akenzander <alexenderwambua2001@gmail.com>
2023-02-27 08:30:54 -0800

    2-stren

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: the string pointer
+ * Return: index
+ */
+int _strlen(char *s);
+{
+	int index;
+	{
+
+		for (index = 0; s(index) != '\0'; index++)
+	}
+			return (index);
+}
