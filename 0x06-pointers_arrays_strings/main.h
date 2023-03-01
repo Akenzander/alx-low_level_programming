85ac13a935607365e5da0ea2ff8bb0b9141ffd1b
akenzander <alexenderwambua2001@gmail.com>
2023-03-01 10:05:43 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,6 +15,7 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+char *leet(char *);
 
 #endif
 
